8e5d88a733faa44a40398e475c30270752ce50ea
xxao <github@bymartin.cz>
2021-10-04 10:38:00 +0200

    Update status

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 # set classifiers
 classifiers = [
-    'Development Status :: 4 - Beta',
+    'Development Status :: 5 - Production/Stable',
     'Programming Language :: Python :: 3 :: Only']
 
 # main setup
